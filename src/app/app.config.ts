d995fe097657ece4f4ffa5bea1a6ec3d44957ee8
Bayron Ocampo <jafethocampo@icloud.com>
2024-05-30 03:20:38 -0600

    angular y sus dependencias actualizados a la version 18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,14 @@
-import { ApplicationConfig, LOCALE_ID, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import  localeCR  from '@angular/common/locales/es';
 
 import { getApp, initializeApp, provideFirebaseApp,  } from '@angular/fire/app';
-import { connectAuthEmulator, getAuth, provideAuth } from '@angular/fire/auth';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
-import { connectFirestoreEmulator, initializeFirestore, persistentLocalCache, persistentMultipleTabManager, provideFirestore } from '@angular/fire/firestore';
+import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager, provideFirestore } from '@angular/fire/firestore';
 import { provideAppCheck, initializeAppCheck, ReCaptchaV3Provider } from '@angular/fire/app-check';
-import { connectStorageEmulator, getStorage, provideStorage } from '@angular/fire/storage';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 import { environment } from '../environments/environment';
 import { routes } from './app.routes';
@@ -20,35 +20,17 @@ registerLocaleData(localeCR);
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withViewTransitions()),
-    importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.config))),
-    importProvidersFrom(provideAppCheck(() => initializeAppCheck(getApp(), {
-        provider: new ReCaptchaV3Provider('6LeEY1QpAAAAAGZURIQFpD05BgnJ4tKf15TdbEER'),
-    }))),
-    importProvidersFrom(provideAuth(() => {
-        const auth = getAuth();
-        // !environment.production ? connectAuthEmulator(auth, 'http://127.0.0.1:9099') : null;
-        return auth;
-    })),
-    importProvidersFrom(provideAnalytics(() => getAnalytics())),
+    provideFirebaseApp(() => initializeApp(environment.config)),
+    provideAppCheck(() => initializeAppCheck(getApp(), {provider: new ReCaptchaV3Provider('6LeEY1QpAAAAAGZURIQFpD05BgnJ4tKf15TdbEER'),})),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
     ScreenTrackingService,
     UserTrackingService,
-    importProvidersFrom(provideFirestore(() => {
-        const firestore = initializeFirestore(getApp(), { localCache: persistentLocalCache({ tabManager: persistentMultipleTabManager() }), experimentalForceLongPolling: true });
-        // !environment.production ? connectFirestoreEmulator(firestore, 'localhost', 8080) : null;
-        return firestore;
-    })),
-    importProvidersFrom(provideStorage(() => {
-        const storage = getStorage();
-        // !environment.production ? connectStorageEmulator(storage, 'localhost', 9199) : null;
-        return storage;
-    })),
+    provideFirestore(() => initializeFirestore(getApp(), { localCache: persistentLocalCache({ tabManager: persistentMultipleTabManager() }), experimentalForceLongPolling: true })),
+    provideStorage(() => getStorage()),
     provideAnimations(),
-    provideToastr({
-        closeButton: true,
-        timeOut: 2500,
-        progressBar: true,
-    }),
+    provideToastr({ closeButton: true, timeOut: 2500, progressBar: true }),
     { provide: LOCALE_ID, useValue: 'es-CR' },
     provideAnimations()
-],
+  ],
 };
